5fcb35047d3e225f6b82db2306934c5a7f426e13
Gartonot <gartonot62@gmail.com>
2025-05-10 01:35:10 +0300

    Создал систему проверки столкновений врага и пуль

---
+++ src/systems/CollisionSystem.ts
@@ -0,0 +1,15 @@
+import { Bullet } from '@/entities/bullet/Bullet';
+import { Enemy } from '@/entities/enemy/Enemy';
+
+export class CollisionSystem {
+    checkBulletEnemyCollisions(bullets: Bullet[], enemies: Enemy[]): void {
+        for (const enemy of enemies) {
+            for (const bullet of bullets) {
+                if (enemy.checkCollision(bullet.x, bullet.y, bullet.radius)) {
+                    enemy.isDead = true;
+                    bullet.isDead = true;
+                }
+            }
+        }
+    }
+}
