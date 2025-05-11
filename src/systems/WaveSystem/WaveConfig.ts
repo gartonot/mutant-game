0af36ad5f04cad8a5ff800067fee398aac395da5
Gartonot <gartonot62@gmail.com>
2025-05-12 01:40:03 +0300

    Подправил конфиг волн

--- src/systems/WaveSystem/WaveConfig.ts
+++ src/systems/WaveSystem/WaveConfig.ts
@@ -6,8 +6,8 @@ export interface IWaveRule {
 }
 
 export const WaveConfigs: IWaveRule[] = [
-    { types: [EnemyType.GRUNT], count: 2 }, // Волна 1: 2 обычных
-    { types: [EnemyType.GRUNT], count: 4 }, // Волна 2: 4 обычных
-    { types: [EnemyType.TANK], count: 2 }, // Волна 3: 2 танка
+    { types: [EnemyType.GRUNT], count: 5 }, // Волна 1: 5 обычных
+    { types: [EnemyType.GRUNT], count: 10 }, // Волна 2: 10 обычных
+    { types: [EnemyType.TANK], count: 4 }, // Волна 3: 4 танка
     { types: [EnemyType.GRUNT, EnemyType.TANK], count: 7 }, // Волна 4: 5 обычных + 2 танка
 ];
